b5a707ec446a1a7e1c166a92b3d4f907b658c478
leendhs <r0943181@student.thomasmore.be>
2024-10-31 11:09:47 +0100

    feat: get, post, put, delete

--- controllers/api/v1/orders.js
+++ controllers/api/v1/orders.js
@@ -1,21 +1,26 @@
 const getAll = (req, res) => {
-  res.send("GET orders");
+  res.send("GET all orders");
+};
+
+const get = (req, res) => {
+  res.send("GET specific order");
 };
 
 const create = (req, res) => {
-  res.send("POST orders");
+  res.send("POST order");
 };
 
 const update = (req, res) => {
-  res.send("PUT orders");
+  res.send("PUT/PATCH order");
 };
 
 const remove = (req, res) => {
-  res.send("DELETE orders");
+  res.send("DELETE order");
 };
 
 module.exports = {
   getAll,
+  get,
   create,
   update,
   remove,
--- routes/api/v1/orders.js
+++ routes/api/v1/orders.js
@@ -1,13 +1,24 @@
-const express = require("express");
-const router = express.Router();
-const ordersController = require("../../../controllers/api/v1/orders");
+var express = require("express");
+var router = express.Router();
 
+// Importeer controllers
+const ordersController = require("../../../controllers/api/v1/orders.js");
+const admin = require("../../../middleware/admin.js");
+
+// Haalt alle bestellingen op, met optionele sortering
 router.get("/", ordersController.getAll);
 
+// Haalt details op voor één specifieke bestelling
+router.get("/:id", ordersController.get);
+
+// Voegt een bestelling toe aan het systeem
 router.post("/", ordersController.create);
 
-router.put("/:id", ordersController.update);
+// Update de status van een bestelling (admin-only)
+router.put("/:id", admin, ordersController.update);
+router.patch("/:id", admin, ordersController.update);
 
-router.delete("/:id", ordersController.remove);
+// Verwijdert een bestelling (admin-only)
+router.delete("/:id", admin, ordersController.remove);
 
 module.exports = router;
