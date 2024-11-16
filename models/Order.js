57e86dcb65b365a5d3d9d4ad5c8a50f46e8245bb
milanaismail <133760937+milanaismail@users.noreply.github.com>
2024-11-16 12:03:04 +0100

    order pagina details

--- models/Order.js
+++ models/Order.js
@@ -1,12 +1,18 @@
-// models/Order.js
 const mongoose = require("mongoose");
 
 const orderSchema = new mongoose.Schema({
   shoeSize: Number,
   laceColor: String,
+  soleColor: String,
+  soleFabric: String,
+  innerShoeColor: String,
+  innerShoeFabric: String,
+  outerShoeColor: String,
+  outerShoeFabric: String,
   customerContact: String,
   status: { type: String, default: "Pending" },
-  createdAt: { type: Date, default: Date.now }
+  createdAt: { type: Date, default: Date.now },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" }, // Reference to User model
 });
 
 module.exports = mongoose.model("Order", orderSchema);
