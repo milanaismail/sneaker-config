9bfceacb8550b51abff3fa569dbb8f367aa414eb
milanaismail <133760937+milanaismail@users.noreply.github.com>
2024-11-16 14:16:46 +0100

    admin login

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,8 +1,10 @@
 import { createRouter, createWebHistory } from "vue-router";
 import OrdersOverview from "../components/OrdersOverview.vue";
 import OrderDetails from "../components/OrdersDetails.vue";
+import AdminLogin from "../components/AdminLogin.vue";
 
 const routes = [
+{ path: "/login", component: AdminLogin }, // Admin Login Page
   { path: "/orders", component: OrdersOverview },
   { path: "/orders/:id", component: OrderDetails },
 ];
