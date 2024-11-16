837beddc44d95a4031b6cafc29ac2928cf8cbd95
milanaismail <133760937+milanaismail@users.noreply.github.com>
2024-11-16 13:44:32 +0100

    typo

--- routes/api/v1/orders.js
+++ routes/api/v1/orders.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const ordersController = require("../../../controllers/api/v1/orders.js");
+const ordersController = require("../../../controllers/api/v1/orders");
+console.log("Orders Controller:", ordersController);
 
-router.get("/", ordersController.getAll); // Overview
+router.get("/", ordersController.getOrdersOverview); 
 router.get("/:id", ordersController.getOrderDetails); // Details
 
 module.exports = router;
