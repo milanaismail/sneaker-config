19302222d26d48bd60909c256d5aacf2691186c2
milanaismail <133760937+milanaismail@users.noreply.github.com>
2024-11-22 13:30:37 +0100

    backend in apart mapje

--- frontend/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-
-const app = createApp(App);
-
-app.use(router); // Register the router
-app.mount("#app"); // Mount the app
