ad42d3086410d76af346185bb485984ca33f6319
milanaismail <133760937+milanaismail@users.noreply.github.com>
2024-11-29 20:22:00 +0100

    phone number weg uit model order

--- models/Order.js
+++ models/Order.js
@@ -12,7 +12,6 @@ const orderSchema = new mongoose.Schema({
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
     email: { type: String, required: true },
-    phone: { type: String, required: true },
     address: { type: String, required: true },
     postalCode: { type: Number, required: true },
     city: { type: String, required: true },
