23203263a71de252e3ea78ab04df48c53b43394d
milanaismail <133760937+milanaismail@users.noreply.github.com>
2024-11-29 20:49:32 +0100

    middleware

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ var app = express();
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
-
+app.use(bodyParser.json()); 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
