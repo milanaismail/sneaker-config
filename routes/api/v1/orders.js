14feb3f8183fc1882b4e677aab6095b475fb6988
milanaismail <133760937+milanaismail@users.noreply.github.com>
2024-11-29 18:21:48 +0100

    local host server toegevoegd

--- routes/api/v1/orders.js
+++ routes/api/v1/orders.js
@@ -6,6 +6,5 @@ router.post("/", ordersController.createOrder);
 router.get("/", ordersController.getOrdersOverview); 
 router.get("/:id", ordersController.getOrderDetails); // Details
 router.put("/:id", ordersController.updateOrderStatus); // Update Status
-router.put("/:id", ordersController.updateOrderWithCustomerInfo); // Update Customer Info
 
 module.exports = router;
