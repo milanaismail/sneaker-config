7d1daa40ac5141c743ab2a3544777d00c488e921
milanaismail <133760937+milanaismail@users.noreply.github.com>
2024-12-02 16:28:08 +0100

    product id

--- controllers/api/v1/orders.js
+++ controllers/api/v1/orders.js
@@ -50,7 +50,9 @@ const getOrderDetails = async (req, res) => {
   try {
     const order = await Order.findById(req.params.id);
     if (!order) return res.status(404).json({ message: "Order not found" });
-
+    order.products.forEach((product, index) => {
+      product.productId = product._id || `Product-${index + 1}`;
+    });
     res.json(order);
   } catch (error) {
     res.status(500).json({ message: "Error retrieving order details", error });
